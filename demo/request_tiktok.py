5b74d8d4dcbdac1ac9dc58ae8e14ef3fb0a0e316
reversecoders <153279254+reversecoderslab@users.noreply.github.com>
2024-05-20 11:47:02 +0200

    Update passport-sdk-version

--- demo/request_tiktok.py
+++ demo/request_tiktok.py
@@ -11,7 +11,6 @@ def tt_common_post_request(session, dev_info, account_info, host, url, body=None
     :param host:
     :param url:
     :param body:
-    :param extra:
     :return:
     """
     url_params = generate_url_common_params(dev_info, extra if extra else None)
@@ -29,7 +28,7 @@ def tt_common_post_request(session, dev_info, account_info, host, url, body=None
         "x-tt-request-tag": "t=0;n=1",
         "x-ss-req-ticket": str(round(time.time() * 1000)),
         "sdk-version": "2",
-        "passport-sdk-version": "5050490",
+        "passport-sdk-version": "6010290",
         "x-vc-bdturing-sdk-version": "2.3.7.i18n",
         "user-agent": dev_info['extra']['userAgent'],
         "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -44,10 +43,13 @@ def tt_common_post_request(session, dev_info, account_info, host, url, body=None
         "X-Ladon": x_ladon
     }
 
+    print(query_str)
     if account_info is not None:
         headers |= {
             'x-tt-token': account_info["x-tt-token"],
             'x-tt-cmpl-token': account_info["cookies"]["cmpl_token"],
+            'x-bd-client-key': account_info["x-bd-client-key"],
+            'x-bd-kmsv': account_info["x-bd-kmsv"],
         }
         response = post_request(session, query_str, headers, post_body=body, cookies=account_info["cookies"])
     else:
@@ -85,7 +87,7 @@ def tt_common_get_request(session, dev_info, account_info, host, url, extra={}):
         "x-tt-request-tag": "t=0;n=1",
         "x-ss-req-ticket": str(round(time.time() * 1000)),
         "sdk-version": "2",
-        "passport-sdk-version": "5050490",
+        "passport-sdk-version": "6010290",
         "x-vc-bdturing-sdk-version": "2.3.7.i18n",
         "user-agent": dev_info['extra']['userAgent'],
         "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
@@ -104,6 +106,8 @@ def tt_common_get_request(session, dev_info, account_info, host, url, extra={}):
         headers |= {
             'x-tt-token': account_info["x-tt-token"],
             'x-tt-cmpl-token': account_info["cookies"]["cmpl_token"],
+            'x-bd-client-key': account_info["x-bd-client-key"],
+            'x-bd-kmsv': account_info["x-bd-kmsv"],
         }
         response = get_request(session, query_str, headers, cookies=account_info["cookies"])
     else:
